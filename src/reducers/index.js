700221ea0a77b90efd7a11f6f43daf76abfc69fd
Alexander Androschuk <a.r.androschuk@gmail.com>
2021-03-27 22:52:07 +0200

    Updating the cart - is completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,17 +2,46 @@ const initialState = {
     parfums: [],
     loading: true,
     error: null,
-    cartItems: [
-        {
-            id: 1,
-            name: 'Montblanc Explorer Eau de Parfum 100ml',
-            img: 'https://splash.com.ua/images/38662-product/montblanc-explorer-%D0%BF%D0%B0%D1%80%D1%84%D1%8E%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%B0%D1%8F-%D0%B2%D0%BE%D0%B4%D0%B0-%D1%84%D0%BE%D1%82%D0%BE-1.jpg',
-            count: 2,
-            total: 1900
-        }
-    ],
+    cartItems: [],
     orderTotal: 3000,
     deliveryCost: 50
+}
+
+const updateCartItems = (cartItems, item, idx) => {
+    
+    if (idx === -1) {
+        return [
+            ...cartItems,
+            item
+        ]
+    }
+
+    return [
+        ...cartItems.slice(0, idx),
+        item,
+        ...cartItems.slice(idx + 1)
+    ]
+}
+
+const updateCartItem = (parfum, item) => {
+
+    const {id, brand, title, concentration, vol, img, price} = parfum;
+
+    if (item) {
+        return {
+            ...item,
+            count: item.count + 1,
+            total: item.total + parfum.price
+        }
+    } else {
+        return {
+            id: id,
+            name: `${brand} ${title} ${concentration} ${vol}`,
+            img: img,
+            count: 1,
+            total: price
+        }
+    }
 }
 
 const reducer = (state = initialState, action) => {
@@ -42,23 +71,14 @@ const reducer = (state = initialState, action) => {
         case "PARFUM_ADDED_TO_CART":
             const parfumId = action.payload;
             const parfum = state.parfums.find(parfum => parfum.id === parfumId);
+            const itemIndex = state.cartItems.findIndex(({id}) => id === parfum.id);
+            const item = state.cartItems[itemIndex];
 
-            const {id, brand, title, concentration, vol} = parfum;
-
-            const newItem = {
-                id,
-                name: `${brand} ${title} ${concentration} ${vol}`,
-                img: parfum.img,
-                count: 1,
-                total: parfum.price
-            }
+            const newItem = updateCartItem(parfum, item);
 
             return {
                 ...state,
-                cartItems: [
-                    ...state.cartItems,
-                    newItem
-                ]
+                cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
             }
         default:
             return state;
